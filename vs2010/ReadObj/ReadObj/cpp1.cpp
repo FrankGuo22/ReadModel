2df83f19d34b0eb8a13a6d1376406fec7c4c44b4
Frank Guo <526720698@qq.com>
2015-06-03 23:31:45 +0100

    Update cpp1.cpp

--- vs2010/ReadObj/ReadObj/cpp1.cpp
+++ vs2010/ReadObj/ReadObj/cpp1.cpp
@@ -10,7 +10,7 @@ lalaafafs;
 set<string> objnasfame;
 string cd;fas
 
-map<string,Material> matname;
+map<strifasng,Material> matname;
 GLfloat dx = 0,dy = 0,dz = 0;afs
 GLfloat ax = 0,ay = 0,az = 0;
 GLint mx = 0,my = 0;
